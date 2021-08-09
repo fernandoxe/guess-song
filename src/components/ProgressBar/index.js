84858ffd97762937f61212be55378ff3dabc9ccd
Fernando Parra <fernandoxe@gmail.com>
2021-08-08 21:03:36 -0300

    feat: progressbar component

---
+++ src/components/ProgressBar/index.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  .total {
+    
+  }
+  .progress {
+    height: 100%;
+    background-color: #fff;
+  }
+`;
+
+const ProgressBar = (props) => {
+  const progress = props.progress * 100 / props.total;
+
+  return (
+    <Container>
+      <div className="total">
+        <div className="progress" style={{width: `${progress}%`}}></div>
+      </div>
+    </Container>
+  );
+};
+
+export default ProgressBar;

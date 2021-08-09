6c605b3b8fad58e530bf51bac1a85033ca30fe02
Fernando Parra <fernandoxe@gmail.com>
2021-08-09 01:48:09 -0300

    feat: display successful songs

--- src/components/Score/index.js
+++ src/components/Score/index.js
@@ -2,13 +2,20 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   width: 80%;
+  .h2 {
+    text-align: center;
+  }
 `;
 
 const Score = (props) => {
   return (
     <Container>
-      <h2>You hit {props.songs.length} song{props.songs.length === 1 ? '' : 's'}</h2>
-      {props.songs.map((song, i) => <div key={i}>{song}</div>)}
+      <h2 className="h2">You hit {props.songs.length} song{props.songs.length === 1 ? '' : 's'}</h2>
+      <div>
+        {props.songs.map((song, i) => {
+          return `${song} ${i === props.songs.length ? '' : ' - '}`;
+        })}
+      </div>
     </Container>
   );
 };

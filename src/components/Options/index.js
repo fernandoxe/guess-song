3ac7ae9eec0b817da0217bb3b82d28e2be555242
Fernando Parra <fernandoxe@gmail.com>
2021-08-11 00:00:59 -0300

    feat: style options

--- src/components/Options/index.js
+++ src/components/Options/index.js
@@ -1,5 +1,18 @@
 import { useState } from 'react';
+import styled from 'styled-components';
 import Button from '../Button';
+
+const Container = styled.div`
+  .options {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+
+    .option:not(:last-child) {
+      margin-bottom: 1rem;
+    }
+  }
+`;
 
 const Options = (props) => {
   const [disabledButtons, setDisabledButtons] = useState(false);
@@ -10,25 +23,27 @@ const Options = (props) => {
   };
 
   return (
-    <ul className="options">  
-      {props.options.map((option, i) => {
-        return (
-          <li
-            key={i}
-            className="option"
-          >
-            <Button
-              className="option-button"
-              correct={props.song === option}
-              onClick={() => handleOptionClick(option, i)}
-              disabled={disabledButtons}
+    <Container>
+      <ul className="options">  
+        {props.options.map((option, i) => {
+          return (
+            <li
+              key={i}
+              className="option"
             >
-              {option}
-            </Button>
-          </li>
-        );
-      })}
-    </ul>
+              <Button
+                className="option-button"
+                correct={props.song === option}
+                onClick={() => handleOptionClick(option, i)}
+                disabled={disabledButtons}
+              >
+                {option}
+              </Button>
+            </li>
+          );
+        })}
+      </ul>
+    </Container>
   );
 };
 

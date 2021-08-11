08e68e590f3a22162832e4fcc5efcc827a4fcbad
Fernando Parra <fernandoxe@gmail.com>
2021-08-11 00:02:42 -0300

    feat: styles and share component

--- src/components/Score/index.js
+++ src/components/Score/index.js
@@ -1,9 +1,17 @@
 import styled from 'styled-components';
+import Share from '../Share';
 
 const Container = styled.div`
   width: 80%;
   .h2 {
     text-align: center;
+    margin: 1.5rem 0;
+  }
+  .successful-songs {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 1.5rem;
   }
 `;
 
@@ -11,11 +19,10 @@ const Score = (props) => {
   return (
     <Container>
       <h2 className="h2">You hit {props.songs.length} song{props.songs.length === 1 ? '' : 's'}</h2>
-      <div>
-        {props.songs.map((song, i) => {
-          return `${song} ${i + 1 === props.songs.length ? '' : ' - '}`;
-        })}
+      <div className="successful-songs">
+        {props.songs.map((song, i) => <div>{song}</div>)}
       </div>
+      <Share songs={props.songs} />
     </Container>
   );
 };

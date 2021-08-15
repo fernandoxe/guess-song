344d4849483660e60171f2105fe2e105b8c9bad6
Fernando Parra <fernandoxe@gmail.com>
2021-08-15 17:59:06 -0300

    feat: points and loading

--- src/components/Score/index.js
+++ src/components/Score/index.js
@@ -3,7 +3,7 @@ import Share from '../Share';
 
 const Container = styled.div`
   width: 80%;
-  .h2 {
+  .h2, .h3 {
     text-align: center;
     margin: 1.5rem 0;
   }
@@ -12,17 +12,27 @@ const Container = styled.div`
     flex-direction: column;
     align-items: center;
     margin-bottom: 1.5rem;
+    font-style: italic;
   }
 `;
 
 const Score = (props) => {
+  const handleCanvasRendered = () => {
+    props.onLoaded();
+  };
+
   return (
     <Container>
       <h2 className="h2">You hit {props.songs.length} song{props.songs.length === 1 ? '' : 's'}</h2>
+      <h3 className="h3">Total points: {props.points}</h3>
       <div className="successful-songs">
         {props.songs.map((song, i) => <div key={i}>{song}</div>)}
       </div>
-      <Share songs={props.songs} />
+      <Share
+        songs={props.songs}
+        points={props.points}
+        onCanvasRendered={handleCanvasRendered}
+      />
     </Container>
   );
 };

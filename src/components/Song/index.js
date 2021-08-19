b6adafcdda82d37a586f06b0efd464063f01694e
Fernando Parra <fernandoxe@gmail.com>
2021-08-19 00:37:24 -0300

    feat: move audio to game

--- src/components/Song/index.js
+++ src/components/Song/index.js
@@ -1,8 +1,6 @@
-import { useRef, useState } from 'react';
 import styled from 'styled-components';
 import Options from '../Options';
 import ProgressBar from '../ProgressBar';
-import config from '../../config';
 import { useTranslation } from 'react-i18next';
 
 const Container = styled.div`
@@ -19,45 +17,22 @@ const Container = styled.div`
 
 const Song = (props) => {
   const { t } = useTranslation();
-  const audioRef = useRef(null);
-  const [audioDuration, setAudioDuration] = useState(null);
-  const [audioProgress, setAudioProgress] = useState(null);
 
   const handleOptionClick = (option, optionNumber) => {
     console.log('Successful:', option === props.song);
-    const leftTime = audioRef.current.duration - audioRef.current.currentTime;
-    audioRef.current.pause();
-    props.onOptionClick(option === props.song, option, optionNumber, leftTime);
+    props.onOptionClick(option === props.song, option, optionNumber);
   };
 
   const handleOptionSelected = () => {
     props.onOptionSelect();
   };
 
-  const handleLoadedMetadata = () => {
-    setAudioDuration(audioRef?.current.duration);
-    props.onLoadedSong();
-  };
-
-  const handleTimeUpdate = () => {
-    setAudioProgress(audioRef?.current.currentTime);
-  };
-
   return (
     <Container>
       <h1 className="h1">{t('What is this song?')}</h1>
-      <div>
-        <audio
-          src={`${config.musicpath}/${props.song}.${config.fileextension}`}
-          autoPlay
-          onLoadedMetadata={handleLoadedMetadata}
-          onTimeUpdate={handleTimeUpdate}
-          ref={audioRef}
-        />
-      </div>
       <div className="progress">
-        { !!audioProgress && audioDuration &&
-          <ProgressBar progress={audioProgress} total={audioDuration} />
+        { !!props.currentTime && props.duration &&
+          <ProgressBar progress={props.currentTime} total={props.duration} />
         }
       </div>
       <Options

e23e11988c98bd22a5cd6f99153addfc91fa9017
Fernando Parra <fernandoxe@gmail.com>
2021-08-20 01:14:06 -0300

    feat: added global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Home from './pages/Home';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
+import { GlobalStyle } from './styles';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Home />
     </ThemeProvider>
   );

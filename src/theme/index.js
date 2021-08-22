acd4b1c21191d012f004909adc4b70074f7ff5ca
Fernando Parra <fernandoxe@gmail.com>
2021-08-22 01:15:07 -0300

    feat: added button props

--- src/theme/index.js
+++ src/theme/index.js
@@ -5,9 +5,15 @@ export const theme = {
     successful: '#66bb6a',
     error: '#ce0000',
     text: '#e1d3bb',
+    textButton: '#e1d3bb',
     background: '#87684c',
+    buttonBorder: 'none',
   },
   font: {
-    family: 'Georama, sans-serif',
+    family: '\'Georama\', sans-serif',
+  },
+  button: {
+    border: 'none',
+    boxShadow: '1px 1px 4px 1px rgba(0, 0, 0, 0.2)',
   },
 };
